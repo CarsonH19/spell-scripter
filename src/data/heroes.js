faf1f8369544af1142c8ff06eaf4583913263061
CarsonH19 <questline.carson@gmail.com>
2024-04-18 19:39:06 +0900

    feat: heroes.js

---
+++ src/data/heroes.js
@@ -0,0 +1,24 @@
+let player = {
+  hero: "",
+};
+
+const PALADIN = {
+  name: "",
+  image: "",
+  audio: {},
+  stats: {
+    strength: 0,
+    dexterity: 0,
+    faith: 0,
+    arcana: 0,
+  },
+  passive: () => {},
+  abilityOne: () => {},
+  abilityTwo: () => {},
+};
+
+// ROGUE
+// PRIESTESS
+// WARLOCK
+// EVOKER
+// RISEN
