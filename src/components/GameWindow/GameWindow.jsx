8fd516a606a9fffe869630477ee5aa4f6a8e5239
CarsonH19 <questline.carson@gmail.com>
2024-04-19 19:30:43 +0900

    feat: GameWindow folder structure

---
+++ src/components/GameWindow/GameWindow.jsx
@@ -0,0 +1,22 @@
+export default function GameWindow() {
+  return (
+    <div>
+      {/* TOP 20% Height */}
+      {/* TOP-LEFT CONTENT - Room Name & Counter */}
+      {/*  TOP-CENTER CONTENT - Initiative Tracker */}
+      {/*  TOP-RIGHT CONTENT - Boss Name */}
+      {/*  TOP-FULL-BOTTOM CONTENT - Boss HP Bar  */}
+
+      {/* MIDDLE 55% Height */}
+      {/* MIDDLE-LEFT CONTENT 35% - Ally Icons */}
+      {/* MIDDLE-CENTER CONTENT 20% - Event Options*/}
+      {/* MIDDLE-RIGHT CONTENT 35% - Enemy Icons */}
+
+      {/* BOTTOM 25% Height */}
+      {/* BOTTOM-LEFT CONTENT 33% - Player Stats & Icon */}
+      {/* BOTTOM-CENTER CONTENT 33% - Actions (Cast, Guard, Item, Flee )*/}
+      {/* When Cast is selected these are replaced with a spell menu */}
+      {/* BOTTOM-RIGHT-CONTENT 33% - Inv, Log, Stats, Tomes, Settings */}
+    </div>
+  );
+}
