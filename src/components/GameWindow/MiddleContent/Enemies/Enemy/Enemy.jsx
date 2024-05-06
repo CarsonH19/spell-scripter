47566057e88f34ab0427e17732bb277720eb73a9
CarsonH19 <questline.carson@gmail.com>
2024-05-06 23:33:38 +0900

    fix: enemy stats not updating on render

--- src/components/GameWindow/MiddleContent/Enemies/Enemy/Enemy.jsx
+++ src/components/GameWindow/MiddleContent/Enemies/Enemy/Enemy.jsx
@@ -3,11 +3,14 @@ import classes from "./Enemy.module.css";
 import Tooltip from "../../../../UI/Tooltip";
 
 import { memo } from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { setTarget } from "../../../../../store/combat-actions";
 import { targetType } from "../../../../../util/targeting";
+import updateStatTotals from "../../../../../store/stats-actions";
 
 const Enemy = ({ enemy }) => {
+  const dispatch = useDispatch();
+
   const order = useSelector((state) => state.combat.order);
   const enemyIndex = order.findIndex((char) => char.id === enemy.id);
   enemy = order[enemyIndex];
@@ -22,6 +25,8 @@ const Enemy = ({ enemy }) => {
     }
   };
 
+  updateStatTotals(dispatch, enemy.id);
+
   return (
     <div className={classes.enemy} onClick={handleSetTarget}>
       <div className={classes.container}>
@@ -34,14 +39,22 @@ const Enemy = ({ enemy }) => {
               <span>{enemy.stats.strength.maxHealth}</span>
             </p>
           </div>
-          <progress max={enemy.stats.strength.maxHealth} value={enemy.currentHealth}>
+          <progress
+            max={enemy.stats.strength.maxHealth}
+            value={enemy.currentHealth}
+          >
             Health Bar
           </progress>
         </div>
         <div className={classes.statusEffects}>
           {enemy.statusEffects.map((effect) => (
-            <Tooltip key={effect.name} title={effect.name} text={effect.description} detail={`Duration: ${effect.duration} rounds`}>
-            <img src="" className={classes.effect} />
+            <Tooltip
+              key={effect.name}
+              title={effect.name}
+              text={effect.description}
+              detail={`Duration: ${effect.duration} rounds`}
+            >
+              <img src="" className={classes.effect} />
             </Tooltip>
           ))}
         </div>
