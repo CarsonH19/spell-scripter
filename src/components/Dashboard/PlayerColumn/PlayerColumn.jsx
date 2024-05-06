000d5d2fed492ed549fb7bac365aea44de9bdcfd
CarsonH19 <questline.carson@gmail.com>
2024-05-06 23:41:52 +0900

    style: PlayerColumn

--- src/components/Dashboard/PlayerColumn/PlayerColumn.jsx
+++ src/components/Dashboard/PlayerColumn/PlayerColumn.jsx
@@ -1,10 +1,11 @@
 import { uiActions } from "../../../store/ui-slice";
 import classes from "./PlayerColumn.module.css";
 
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
 export default function PlayerColumn() {
   const dispatch = useDispatch();
+  const player = useSelector((state) => state.player);
 
   const handleOpenModal = (modal) => {
     dispatch(uiActions.toggle({ modal: "modalIsVisible" })); // set to true
@@ -15,26 +16,29 @@ export default function PlayerColumn() {
     <div className={classes.column}>
       <div className={classes.player}>
         <div>
-          <p>Player Name</p>
-          <p>Level: 0</p>
-          <p>Health: 0</p>
-          <p>Mana: 0</p>
-        </div>
-        <div className={classes.image}></div>
-        <div>
-          <p>Strength: 0</p>
-          <p>Dexterity: 0</p>
-          <p>Faith: 0</p>
-          <p>Arcana: 0</p>
+          <p>{player.name}</p>
+          <p>Level: {player.level}</p>
+
+          {/* <div className={classes.image}></div> */}
+
+          <p>Strength: {player.stats.strength.totalStrength}</p>
+          <p>Agility: {player.stats.agility.totalAgility}</p>
+          <p>Arcana: {player.stats.arcana.totalArcana}</p>
         </div>
       </div>
       <hr />
       <div className={classes.menu}>
-        <button onClick={() => handleOpenModal("spellbookModal")}>Spellbook</button>
-        <button onClick={() => handleOpenModal("inventoryModal")}>Inventory</button>
+        <button onClick={() => handleOpenModal("spellbookModal")}>
+          Spellbook
+        </button>
+        <button onClick={() => handleOpenModal("inventoryModal")}>
+          Inventory
+        </button>
         <button onClick={() => handleOpenModal("heroesModal")}>Heroes</button>
         <button onClick={() => handleOpenModal("questsModal")}>Quests</button>
-        <button onClick={() => handleOpenModal("settingsModal")}>Settings</button>
+        <button onClick={() => handleOpenModal("settingsModal")}>
+          Settings
+        </button>
       </div>
     </div>
   );
