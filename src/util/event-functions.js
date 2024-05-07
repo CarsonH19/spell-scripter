fe8275c4c27be59304ef965b6a9c0c447e0af039
CarsonH19 <questline.carson@gmail.com>
2024-05-07 16:43:45 +0900

    refactor: trap event handling

--- src/util/event-functions.js
+++ src/util/event-functions.js
@@ -15,28 +15,73 @@ const eventFunctions = {
     const order = store.getState().combat.order;
 
     for (let i = 0; i < order.length; i++) {
-      const stats = order[i].stats;
+      let success;
 
-      let highestStat = Math.max(stats.strength.totalStrength, stats.agility.totalAgility);
-      console.log(highestStat);
-      if (order[i].identifier === "PLAYER") {
-        if (text === "Strength") {
-          highestStat = stats.totalStrength;
-        } else if (text === "Agility") {
-          highestStat = stats.totalAgility;
-        }
+      if (order[i].identifier === "PLAYER" && text === "Strength") {
+        // Use players choice stat
+        success = trapSuccessChance(order[i], 12, "STRENGTH");
+      } else if (order[i].identifier === "PLAYER" && text === "Agility") {
+        success = trapSuccessChance(order[i], 12, "AGILITY");
+      } else {
+        // Heroes choose their highest stat
+        success = trapSuccessChance(order[i], 12);
       }
 
-      const successChance = roll20(highestStat);
-
-      if (successChance > 10) {
-        console.log("SUCCESS", order[i]);
-      } else {
-        console.log("FAILURE", order[i]);
+      if (!success) {
         changeHealth(dispatch, order[i], "DAMAGE", 15);
       }
+
+      console.log("SUCCESS", success);
     }
   },
 };
+
+function trapSuccessChance(character, difficulty, stat) {
+  const stats = character.stats;
+  let highestStat;
+
+  if (stat && stat === "STRENGTH") {
+    highestStat = stats.strength.totalStrength;
+  } else if (stat && stat === "AGILITY") {
+    highestStat = stats.agility.totalAgility;
+  } else {
+    Math.max(stats.strength.totalStrength, stats.agility.totalAgility);
+  }
+
+  const successChance = roll20(highestStat);
+
+  if (successChance > difficulty) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+// SPIKE_WALLS_STRENGTH: (dispatch, text) => {
+//   const order = store.getState().combat.order;
+
+//   for (let i = 0; i < order.length; i++) {
+//     const stats = order[i].stats;
+
+//     let highestStat = Math.max(stats.strength.totalStrength, stats.agility.totalAgility);
+//     console.log(highestStat);
+//     if (order[i].identifier === "PLAYER") {
+//       if (text === "Strength") {
+//         highestStat = stats.totalStrength;
+//       } else if (text === "Agility") {
+//         highestStat = stats.totalAgility;
+//       }
+//     }
+
+//     const successChance = roll20(highestStat);
+
+//     if (successChance > 10) {
+//       console.log("SUCCESS", order[i]);
+//     } else {
+//       console.log("FAILURE", order[i]);
+//       changeHealth(dispatch, order[i], "DAMAGE", 15);
+//     }
+//   }
+// },
 
 export default eventFunctions;
