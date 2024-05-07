a8a4d792533208a77551751073c05aad2349ddf5
CarsonH19 <questline.carson@gmail.com>
2024-05-07 12:56:21 +0900

    feat: enemies defeated logic

--- src/components/Modals/RoomSummary/RoomSummaryModal.jsx
+++ src/components/Modals/RoomSummary/RoomSummaryModal.jsx
@@ -1,14 +1,27 @@
+import { useSelector } from "react-redux";
 import classes from "./RoomSummaryModal.module.css";
 
 export default function RoomSummaryModal() {
+  const enemies = useSelector((state) => state.dungeon.contents.enemies);
+
   return (
     <div className={classes.summary}>
       <h1>Room Cleared!</h1>
       <div className={classes.container}>
-        <div>QTE</div>
         <div>TOME PROGRESS BAR</div>
-        <div>EVENT</div>
-        <div>ENEMIES DEFEATED</div>
+        <div>QTE QUESTIONS</div>
+        <div>EVENTS</div>
+        {enemies.length > 0 && (
+          <div>
+            <h4>Enemies Defeated</h4>
+            <ul>
+            {enemies.map((enemy) => {
+              return <li key={enemy.id}>{enemy.name}</li>;
+            })}
+          </ul>
+          </div>
+          
+        )}
         <div>ITEMS FOUND</div>
       </div>
     </div>
