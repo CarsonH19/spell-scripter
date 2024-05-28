30e64f77338f45eb2fb0c695097de8e29f96b508
CarsonH19 <questline.carson@gmail.com>
2024-05-29 03:57:15 +0900

    feat: character name displayed below initiative

--- src/components/GameWindow/TopContent/InitiativeTracker/InitiativeTracker.jsx
+++ src/components/GameWindow/TopContent/InitiativeTracker/InitiativeTracker.jsx
@@ -5,15 +5,24 @@ import MemoizedIcon from "./Icon";
 
 export default function InitiativeTracker() {
   const order = useSelector((state) => state.combat.order);
+  const id = useSelector((state) => state.combat.isCharacterTurn);
+
+  const findCharacterById = (id) => {
+    return order.find((char) => char.id === id);
+  };
+
+  let character = findCharacterById(id);
+
+  if (!character) {
+    character = ''; 
+  }
 
   return (
     <div className={classes.tracker}>
       {order.map((character, index) => (
-        <MemoizedIcon
-          key={index}
-          character={character}
-        />
+        <MemoizedIcon key={index} character={character} />
       ))}
+      <p>{character.name}</p>
     </div>
   );
 }
