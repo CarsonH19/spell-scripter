8a24ab4a8c16e3ff237dd970f1d42f2d2f310787
CarsonH19 <questline.carson@gmail.com>
2024-06-04 11:29:10 +0900

    feat: tome data

--- src/components/Dashboard/TomeColumn/TomeColumn.jsx
+++ src/components/Dashboard/TomeColumn/TomeColumn.jsx
@@ -1,20 +1,23 @@
 import classes from "./TomeColumn.module.css";
 
+// import tomes from "../../../data/tomes";
+
 export default function TomeColumn() {
   return (
     <div className={classes.column}>
       <h1>Tomes</h1>
       <p>Mastery Points: 0</p>
       <div className={classes.filter}>
+        {/* Add a filter button logic */}
         <button>All</button>
         <button>Mastered</button>
         <button>Incomplete</button>
       </div>
       <div className={classes.tomes}>
-        {/* Add a filter button logic */}
         {/* Map each tome into a div element  */}
         {/* Add text into each div (Name) (Mastery %) */}
         {/* Allow overflow scrollbar */}
+
       </div>
     </div>
   );
---
+++ src/data/tomes.js
@@ -0,0 +1,113 @@
+export const tomes = [
+  // INTRODUCTION
+  {
+    name: "Introduction",
+    unlocked: true,
+    complete: false,
+    lesson: [
+      {
+        page: 1,
+        type: "INFO",
+        title: "Welcome to JavaScript!",
+        text: [
+          `In this course, we'll learn the basic concepts of JavaScript - one of the most popular programming languages that makes websites dynamic and interactive. With JavaScript, you can also create mobile apps and games, process data, and much more!`,
+        ],
+      },
+      {
+        page: 2,
+        type: "QUESTION",
+        title: "",
+        question: "What is JavaScript?",
+        answers: [
+          "One of the most popular programming languages in the world.",
+          "A web browser",
+        ],
+      },
+      {
+        page: 3,
+        type: "INFO", // change type to "EDITOR"?
+        title: "Output",
+        text: [
+          "The console is part of the web browser and allows you to log messages, run JavaScript code, and see errors and warnings.",
+          `Let's kick things off by creating a program that displays "Hello World!" to the console using the <b>console.log()</b> function.`,
+        ],
+        codeEditor: true, // add code editor details here
+      },
+      {
+        page: 4,
+        type: "QUESTION",
+        title: "",
+        question: "Which function is used to generate output to the console?",
+        answers: ["print()", "write()", "console.log()"],
+      },
+      {
+        page: 5,
+        type: "INFO",
+        title: "Text",
+        text: ["To use text in JavaScript, we need to enclose it in quotes:"],
+        codeEditor: true,
+      },
+      {
+        page: 6,
+        type: "QUESTION",
+        title: "",
+        question: `How do you output "Game Over" to the console?`,
+        answers: [
+          `console.log("Game Over");`,
+          `console.log{"Game Over"};`,
+          `console.log(Game Over);`,
+          `console."Game Over";`,
+        ],
+      },
+      {
+        page: 7,
+        type: "INFO",
+        title: "Output",
+        text: [
+          "You can use the console.log() function as many times as you want. Each statement outputs text from a new line.",
+          `A computer program is a list of "instructions" to be "executed" by a computer.`,
+          `In a programming language, these programming instructions are called statements.`,
+        ],
+        codeEditor: true,
+      },
+      {
+        page: 8,
+        type: "INFO",
+        title: "Numbers",
+        text: [`When working with numbers, quotes are not needed.`],
+        codeEditor: true,
+      },
+      {
+        page: 9,
+        type: "QUESTION",
+        title: "",
+        question: `How do you output the number 19 to the console?`,
+        answers: [
+          `console.log(19);`,
+          `console.log{"19"};`,
+          `console.log("19");`,
+          `console.19;`,
+        ],
+      },
+      {
+        page: 10,
+        type: "INFO",
+        title: "",
+        question: `How do you output the number 19 to the console?`,
+        answers: [
+          `Amazing, you have taken the first step! Let's summarize what you've just learned:`,
+          `The console is part of the web browser and allows you to log messages, run JavaScript code, and see errors and warnings.`,
+          `console.log() is used to display a text to the console`,
+        ],
+      },
+    ],
+  },
+];
+
+// Add a preview page to show mastery progress
+// // add a "Overview Section"
+// // add a progress bar with %
+// // add a study button to the preview page
+
+// Add a complete button on the last page.
+// add a page counter 
