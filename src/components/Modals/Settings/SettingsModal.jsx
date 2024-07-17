fb013b3f65f2874b5cb6dc5dd2431845b23b63d0
CarsonH19 <questline.carson@gmail.com>
2024-07-17 23:56:12 +0900

    fix: clear narrative on exit dungeon

--- src/components/Modals/Settings/SettingsModal.jsx
+++ src/components/Modals/Settings/SettingsModal.jsx
@@ -1,17 +1,31 @@
 import { useDispatch } from "react-redux";
 import { uiActions } from "../../../store/ui-slice";
+import { logActions } from "../../../store/log-slice";
 
 export default function SettingsModal() {
   const dispatch = useDispatch();
 
   const handleExitDungeon = () => {
+    // Open dashboard
     dispatch(
       uiActions.changeUi({ element: "dashboardIsVisible", visible: true })
     ); 
+
+    // Close game window
     dispatch(
       uiActions.changeUi({ element: "gameWindowIsVisible", visible: false })
     );
+
+    // Close modal
     dispatch(uiActions.changeUi({ element: "modalIsVisible", visible: false }));
+
+    // Clear any lingering narrations
+    dispatch(logActions.updateLogs({ change: "UNPAUSE" }));
+    dispatch(logActions.updateLogs({ change: "CLEAR" }));
+    console.log("CLEAR")
+
+    // Toggle off danger
+    dispatch(dungeonActions.dangerToggle({ danger: false }));
   };
 
   return (
