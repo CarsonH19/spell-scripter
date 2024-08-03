fe8afbcf9b7d0aa8e7fcffb26b8d025422607ca7
CarsonH19 <questline.carson@gmail.com>
2024-08-03 12:16:11 +0900

    feat: audio logic

--- src/data/audio/music.js
+++ src/data/audio/music.js
@@ -1,8 +1,36 @@
-import { Howl, Howler } from 'howler';
+import { Howl, Howler } from "howler";
 
 // Background music
-export const backgroundMusic = new Howl({
-  src: ['/sounds/background-music.mp3'],
-  loop: true,
-  volume: 0.5
-});
+export const backgroundMusic = {
+  dashboard: new Howl({
+    src: ["/sounds/background-music.mp3"],
+    loop: true,
+    volume: 0.5,
+  }),
+  tome: new Howl({
+    src: ["/sounds/background-music.mp3"],
+    loop: true,
+    volume: 0.5,
+  }),
+  catacomb: new Howl({
+    src: ["/sounds/background-music.mp3"],
+    loop: true,
+    volume: 0.5,
+  }),
+  wailingWarrens: new Howl({
+    src: ["/sounds/background-music.mp3"],
+    loop: true,
+    volume: 0.5,
+  }),
+  thievesRuin: new Howl({
+    src: ["/sounds/background-music.mp3"],
+    loop: true,
+    volume: 0.5,
+  }),
+  // boss battles
+  // candlelight shrine
+  // bonevault
+  // laughing coffin
+  // ambush
+  // trap 
+};
--- src/data/audio/sound-effects.js
+++ src/data/audio/sound-effects.js
@@ -8,4 +8,51 @@ export const soundEffects = {
     src: ['/sounds/win.mp3'],
     volume: 1.0
   })
+  // // Dungeon
+  // enter dungeon
+  // room summary
+  // next room
+
+  // // Attributes
+  // Inc./Dec. Attribute
+
+  // // Hero
+  // Hero Select
+
+  // // Spellbook
+  // Select Skill
+  // Reset Mastery Points
+
+  // // Inventory
+  // Consumables
+  // Eat
+  // Drink
+
+  // Equipment
+  // Attune
+  // Unattune
+
+
+  // // Combat
+  // Spells
+  // Attacks
+  // Guard
+  // Abilities
+  // injury
+  // defeat
+  // enemy spawn
+
+  // // tome  
+  // open / close
+  // next page
+  
+  // // Player
+  // level up 
+  // master tome
+
+  // QTE
+  // open
+  // answer
+  // success
+  // incorrect
 };
